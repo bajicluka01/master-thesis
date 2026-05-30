corrected norm computation

face_width.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def sgn(x):
+    if x >= 0:
+        return 1
+    else: 
+        return -1
+
 def fw(a, b, c):
     #if c == 0: # speculative
     #    return min(a, b)
@@ -14,20 +20,28 @@ def fw(a, b, c):
         k1 = int(np.ceil(tmp))
         k2 = int(np.floor(tmp))
 
-        #s1 = abs(max(k1*a+l*c, l*b))
-        #s2 = abs(max(k2*a+l*c, l*b))
+        c1 = k1*a+l*c
+        c2 = k2*a+l*c
+        lb = l*b
 
-        s1 = max(abs(k1*a+l*c), l*b)
-        s2 = max(abs(k2*a+l*c), l*b)
+        if sgn(c1) != sgn(lb):
+            d1 = abs(c1) + abs(lb)
+        else:
+            d1 = max(abs(k1*a+l*c), lb)
 
-        if s1 < upper_bound:
-            upper_bound = s1
-        if s2 < upper_bound:
-            upper_bound = s2
+        if sgn(c2) != sgn(lb):
+            d2 = abs(c2) + abs(lb)  
+        else:
+            d2 = max(abs(k2*a+l*c), lb)
+
+        if d1 < upper_bound:
+            upper_bound = d1
+        if d2 < upper_bound:
+            upper_bound = d2
     return upper_bound
 
 if __name__ == "__main__":
-    a = 9
+    a = 18
     b = 1
-    c = 4
+    c = 11
     print(fw(a, b, c))